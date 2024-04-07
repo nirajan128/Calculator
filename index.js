bf064a0f705bb8ff69bfc8361a8d34dbf02d175c
Nirajan <nirajanshrestha672@gmail.com>
2024-04-07 10:45:17 -0400

    added Number systems fucntions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let allButton = document.querySelectorAll(".btn");
 
 allButton.forEach(button => { //2.for each button in calculator get their value
     button.addEventListener("click", ()=>{
-          let display = document.getElementById("Results")//3.get the input filed to display result
+          let display = document.getElementById("Results");//3.get the input filed to display result
 
           let btnValue = button.textContent; //4.get the value of the button click
           
@@ -12,20 +12,26 @@ allButton.forEach(button => { //2.for each button in calculator get their value
           //a space, the expression clicked, and a space
           if (btnValue == "/" || btnValue == "*" || btnValue == "+" || btnValue == "-" || btnValue == "." ){
                 
-                 display.value += " " + btnValue + " ";  
+                 display.value += btnValue;  
                 /* display.value = display.value +" " +btnValue +" " *///in more simple way
 
           }else if (btnValue == "DEL"){//7. if the btn value is DEL
-                let currentValue = display.value //gets the current value
-                display.value = currentValue.slice(0,-1) //slices the current value from last
+                let currentValue = display.value; //gets the current value
+                display.value = currentValue.slice(0,-1); //slices the current value from last
 
           }else if(btnValue == "="){ //8. if btn value is = perform a math operation
-              const fullExp = display.value //11.get the expression value
+              const fullExp = display.value; //11.get the expression value
               console.log( fullExp)
 
               try {
-                let finalResult = calculate(fullExp);
-                display.value = finalResult
+                const finalResult = calculate(fullExp);
+                display.value = finalResult;
+                let binary = finalResult.toString(2);
+                let hexaDecimal = finalResult.toString(16);
+                let octal = finalResult.toString(8);
+                console.log(binary)
+                console.log(hexaDecimal)
+                console.log(octal)
 
               } catch (error) {
                 console.log("error")
@@ -33,11 +39,11 @@ allButton.forEach(button => { //2.for each button in calculator get their value
           }
 
           else if (btnValue == "AC"){ //9.if btnvalue is AC set its value to 0
-            display.value = 0 
+            display.value = "" 
           }
 
           else{ //10. else convert the btn value to integer and set display value to the integer
-            convertNum = parseInt(btnValue)
+            convertNum = parseFloat(btnValue)
             display.value+=convertNum
           }
          
@@ -48,16 +54,15 @@ allButton.forEach(button => { //2.for each button in calculator get their value
 //12.Make a function that performs math calculation on the expression
 function calculate(fullExp){
     
-    let tokens = fullExp.split(" ") //13.splits the fullexp and make it into a array of sub strting
-    let result  = parseFloat(tokens[0])
-
+    let tokens = fullExp.match(/[+\-*\/]|\d+(\.\d+)?/g);//13.splits the fullexp and make it into a array of sub strting
+    let result  = parseFloat(tokens[0]) //first number
     console.log(tokens)
 
 
     for (i=1; i<tokens.length; i+=2){ //14. use a loop to get the individual expression 
         let operator = tokens[i] //16. get each expression only, where i = i+1
         let operand = parseFloat(tokens[i + 1]) //15.get each numbers only, where i = i+2, convert it into float value
-       
+        
         console.log(operand)
         console.log(operator)
         
@@ -90,6 +95,10 @@ function calculate(fullExp){
       return result
 }
 
+
+function NumberSystem(){
+
+}
 console.log(allButton)
 
 
